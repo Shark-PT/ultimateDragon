7d47b24a0b448e859a1c43c3aa41ef0c4a68bda1
SharkPT <jccreis@hotmail.com>
2023-06-18 15:11:32 +0100

    Terminada a nav.php

--- content/nav.php
+++ content/nav.php
@@ -1,2 +1,49 @@
-<nav></nav>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark text-light">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item">
+            <!-- Menu inicio-->
+          <a class="nav-link text-light "  href="index.php?p=inicio">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="index.php?p=mestre">O Mestre</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="index.php?p=academia">Academia</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link text-light dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Modalidades
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="index.php?p=kempo">Kempo</a></li>
+            <li><a class="dropdown-item" href="index.php?p=treino-funcional">Treino Funcional</a></li>
+          </ul>
+        </li>
+        
+        <li class="nav-item dropdown">
+          <a class="nav-link text-light dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Equipas
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="index.php?p=equipa-competicao">Equipa de competição Kempo</a></li>
+            <li><a class="dropdown-item" href="index.php?p=dragonkids">DragonKids</a></li>
+          </ul>
+        </li>
+<!--        
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+-->
+        <li class="nav-item">
+          <a class="nav-link text-light" href="index.php?p=contacto">Contacto</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 <main>
