2b70de79edec000d8b76894aa18a3d8b09144e2a
SharkPT <jccreis@hotmail.com>
2023-06-18 16:55:16 +0100

    páginal inicio concluida com redes sociais

--- content/header.php
+++ content/header.php
@@ -1,3 +1,3 @@
 <header class="text-center bg-light">
-    <img src="img/banner.jpg" alt="Banner Ultimate Dragon" class="img-fluid p-3">
+    <img src="img/banner.jpg" width="900" height="205" alt="Banner Ultimate Dragon" class="img-fluid p-3">
 </header>
--- content/pages/inicio.php
+++ content/pages/inicio.php
@@ -1,1 +1,27 @@
-INICIO
+<div class="row px-3">
+    <div class="col-12 col-xl-6 p-3 my-auto">
+        <p>Aviso</p>
+        <p>Apenas para pessoas motivadas</p>
+        <P>Entrando aqui, estás a concordar em dao o teu maximo.</P>
+        <p>Não há desculpas nem segundas hipóteses.</p>
+        <p>Todos os dias, todos os momentos, são novas oportunidades para seres mais e melhor.</p>
+        <p>Treina e terás resultados.</p>
+        <p>No fim do dia, olha para o espelho e pergunta se deste o teu melhor.</p>
+        <p>Se não tiveres este nivel de compromisso, não desperdices o teu tempo, nem o nosso.</p>
+        <p>Honra o teu sacrificio e o nosso trabalho</p>
+        <p>Ultimate Dragon, A arte de combater</p>
+        <div class="row text-center social-icons mx-4">
+            <div class="col-0 col-md-1"></div>
+            <div class="col-4 col-md-2 mb-3">
+                <a href="https://www.facebook.com/ultimatedragon.pt" class="icon-facebook" target="_blank"><i class="bi bi-facebook"></i></a>
+            </div>
+            <div class="col-4 col-md-2 mb-3">
+                <a href="https://www.instagram.com/ultimatedragonfight/" class="icon-instagram" target="_blank"><i class="bi bi-instagram"></i></a>
+            </div>
+            <div class="col-0 col-md-1"></div>
+        </div>
+    </div>
+    <div class="col-12 col-xl-6 p-3">
+        <img src="img/Udragon_Logo.jpg" width="500" height="500" alt="Ultimate Dragon">
+    </div>
+</div>
