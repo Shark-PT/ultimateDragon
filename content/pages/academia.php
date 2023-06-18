b3a1a102e239d87091bd7cf51de5375e179baac4
SharkPT <jccreis@hotmail.com>
2023-06-18 17:40:50 +0100

    ficheiro mestre.php e academia.php completados

---
+++ content/pages/academia.php
@@ -0,0 +1,52 @@
+<div class="row px-3">
+    <div class="col-12 col-xl-6 p-3 my-auto">
+        <p>
+            <b>Historia</b>
+            <p>
+                A academia encontra-se aberta desde o dia 8 de Janeiro de 2013, como DAMTM (delegação de Artes Marciais Tiago Mendes).
+                Inicialmente a mesma apenas estava direcionada para a pratica de King Fu Tradicional e Sanda.
+            </p>
+            <p>
+                Competindo nas duas modalidades acima mencionadas, em 10 competições, conseguimos conquistar 10 titulos de campeões mundiais e 17 nacionais.
+            </p>
+            <p>
+                A partir de 2014, a academia começou a "viver" novas experiências devido a iniciação da pratica de outras artes marciais.
+            </p>
+            <p>
+                Em 2017 foi criada a UltimateDragon, com o objectivo de ser uma casa aberta a todas as artes marciais, desportos de combate e sistema de defesa pessoal.
+            </p>
+            
+        </p>
+        <p>
+            <b>Regras</b>
+            <ol>
+                <li>Apenas usar o que aprende em legitima defesa e para fins pacificos. O uso fora da escola para ataque, exibição ou intimidação é expressamente proibido</li>
+                <li>Respeitar a arte marcial e toda a sua envolvencia</li>
+                <li>Respeitar o mestre</li>
+                <li>Respeitar os colegas de treino</li>
+                <li>Respeitar o espaço e o material de treino</li>
+                <li>Obedecer a todas as ordens dos ateltas mais graduados ou mais antigos</li>
+                <li>Treinar com empenho e concentração</li>
+                <li>Estar atento durante a demonstração e/ou exposição de algum exercicio</li>
+                <li>Sempre que tiver alguma duvida, perguntar as vezes que forem necessárias até entender o que relamente é pedido</li>
+                <li>Sempre que quiser falar de alguma coisa, que não seja sobre os treinos,m é necessário levantar a mão no ar e aguardar qu o mestre dê permissão para falar.</li>
+                <li>Não trazer os "problemas de fora" para o trino.</li>
+                <li>Pedir sempre que quiser beber, comer, ir à casa de banho ou fazer pausa no treino.</li>
+                <li>Retirar qualquer tipo de adereço(brincos, pulseiras, colares, rellógios...)</li>
+                <li>Não se esquecer do material.</li>
+                <li>Não se esquecer do treino.</li>
+                <li>Não chegar atrasado.</li>
+                <li>Não ser preguiçoso.</li>
+                <li>Não ser resmungão.</li>
+                <li>Em nenhuma situação podem ficar "amuados".</li>
+                <li>Informar sempre o mestre de tudo o que diz respeito à escola.</li>
+                <li>Proibido o uso de telemóvel sem permissão.</li>
+                <li>O treino é realizado descalço ou com calçado apropriado, desde que devidamente aceite pelo mestre</li>
+
+            </ol>
+        </p>
+    </div>
+    <div class="col-12 col-xl-6 p-3">
+        <img src="img/academia/historia.jpg" width="500" height="500" alt="academia">
+    </div>
+</div>
---
+++ content/pages/mestre.php
@@ -0,0 +1,28 @@
+<div class="row px-3">
+    <div class="col-12 col-xl-6 p-3 my-auto">
+        <p>Pela mão do ser avô, começou a jogar futebol em 2000, no entanto por motivos de força maior abandonou esta modalidade em Abril de 2002. De Abril a setembro desse mesmo ano 
+            passou por algumas modalidades até que , por fim, iniciou a pratica de King Fu Tradicional.
+            Em Janeiro de 2003,m ingressou no Sanda (deporto de combate de Kung Fu). De 2003 a 2010, FPAMC obteve 12 titulos nacionais, 10 internacionais e 5 ibéricos.
+        </p>
+        <p>Depois de todos os titulos, definiu novos objectivos e ingressou na FPL (Federação Portuuesa de Lohan Tao, que engloba todas as Artes Marciais e Deportos de Combate). 
+            De 2003 até 2015 treinou Karaté Kyokushinkay.
+        </p>
+        <p>
+            Em 2013 concluiu o curso de Treinador de Nivel I da Federação Portuguesa de Lohan Tao, o que lhe permitiu a criação da sua academia.
+        </p>
+        <p>
+            Em 2015, boxe, taekwondo, jiu-jitsu, wrestling, kickboxing e MMA foram as novas praticas que iniciou.
+        </p>
+        <p>
+            até 2017 obteve 19 titulos nacionais, 13 internacionais, 7 ibéricos e 15 mundiais
+        </p>
+        <p>
+            em 2017 fundou a UltimateDragon onde actualmente é treinador de nivel II e atleta.
+        </p>
+        <p>
+            No final de 2017, apesar da derrota por decisão dividida, participou no primeiro evento profissional de MMA do Porto Fightclub.
+        </p>
+    </div>
+    <div class="col-12 col-xl-6 p-3">
+        <img src="img/Tiago_Mendes.jpg" width="500" height="500" alt="Mestre Tiago Mendes">
+    </div>
