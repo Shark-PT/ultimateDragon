dc90c7d124e1e10836794c7dc8a368ea30f907e5
SharkPT <jccreis@hotmail.com>
2023-06-18 10:30:16 +0100

    Terminado footer.php

--- content/footer.php
+++ content/footer.php
@@ -1,5 +1,7 @@
 </main>
-<footer></footer> 
+<footer class="container-fluid bg-dark text-light text-center p-4">
+    &copy; <?=date('Y');?> José Reis
+</footer> 
 <!--BOOTSTRAP JS-->
 <script src="js/bootstrap.bundle.min.js"></script>
 <!--MEU JS-->
